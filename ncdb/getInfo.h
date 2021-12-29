31a755265c28c8a4cd14dd8df06c7dab533b6694
Kleidi Bujari <kleidib@gmail.com>
2021-12-28 20:24:36 -0500

    meminfo, processes done

--- ncdb/getInfo.h
+++ ncdb/getInfo.h
@@ -1,6 +1,14 @@
 #ifndef GETINFO_H
 #define GETINFO_H
 
-int getTemp();
+#include <curl/curl.h>
+#include <sys/sysinfo.h>
+
+// System info
+unsigned long *getMem();
+unsigned long getProcesses();
+
+
+char *getWeatherInfo();
 
 #endif
--- ncdb/main.c
+++ ncdb/main.c
@@ -1,31 +1,19 @@
 #include <stdio.h>
-#include <curl/curl.h>
+#include <unistd.h>
+#include <json-c/json.h>
 
 #include "getInfo.h"
- 
-int main(void)
-{
-  CURL *curl;
-  CURLcode res;
- 
-  curl = curl_easy_init();
-  if(curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "https://api.openweathermap.org/data/2.5/weather?id=6167865&appid=2d7b0899966721fd30b1be1f2511e55a&units=metric");
-    /* example.com is redirected, so we tell libcurl to follow redirection */
-    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
- 
-    /* Perform the request, res will get the return code */
-    res = curl_easy_perform(curl);
-    /* Check for errors */
-    if(res != CURLE_OK)
-      fprintf(stderr, "curl_easy_perform() failed: %s\n",
-              curl_easy_strerror(res));
- 
-    /* always cleanup */
-    curl_easy_cleanup(curl);
-  }
 
-  printf("%d\n", getTemp());
-  return 0;
+int main(int argc, char const *argv[]){
+
+    /*while (1){
+      unsigned long *n = getMem();
+      printf("Memory:     %lu / %lu\n", *n, *(n+1));
+      //printf("Processes:  %lu\n", getProcesses());
+      sleep(1);
+    }*/
+
+    printf(getWeatherInfo("2d7b0899966721fd30b1be1f2511e55a", "6167865"));
+    
+    return 0;
 }
-
